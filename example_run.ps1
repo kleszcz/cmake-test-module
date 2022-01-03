59c64fbca783c443b2fe714bf4863f59df5c86ee
Jan Kleszczyński <jankleszcz@gmail.com>
2022-01-03 20:51:40 +0100

    simple module with test case

--- example_run.ps1
+++ example_run.ps1
@@ -2,7 +2,12 @@ $env:CMAKE_GENERATOR="Ninja"
 $env:CMAKE_BUILD_TYPE="Debug"
 $env:DESTDIR="./install"
 
-cmake -B build -S .
+echo "[Configure]..."
+cmake -B build -S . -DBUILD_TESTS=OFF --log-level=VERBOSE
+echo "[Build]..."
 cmake --build build
-ctest --test-dir build
-cmake --install build --prefix "/install-prefix" --component test-module
+# ctest --test-dir build
+echo "[Install]..."
+cmake --install build --prefix "/" --component test-module
+echo "[Build tests]..."
+cmake -B build -S . -DCMAKE_PREFIX_PATH="$pwd/install" -DBUILD_TESTS=ON --log-level=VERBOSE
